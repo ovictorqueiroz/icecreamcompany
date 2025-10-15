dfcba78c61208ec924abe42bbb811f87760abf08
ovictorqueiroz <victorbr442@gmail.com>
2025-10-14 23:27:59 -0300

    Criando página de login

--- pages/login.php
+++ pages/login.php
@@ -5,8 +5,11 @@ include $_SERVER['DOCUMENT_ROOT'].'/icecreamcompany/components/header.php';?>
 <main>
     <h1>Login</h1>
     
-    <form action="" method = "post">
-        <label for="userName">Nome: </label>
+    <form action="../actions/verifylogin.php" method = "post">
+        <label for="userEmail">Email: </label> <input type="email" name="userEmail" placeholder = "Digite seu e-mail"> <br><br>
+        <label for="userPass">Senha: </label> <input type="password" name="userPass" placeholder = "Digite sua senha"> <br><br>
+        <input type="submit" name="btnEnviar">
+        <p>Não possui uma conta? <a href = "../pages/cadastro.php">Cadastre-se</a></p>
     </form>
 </main>
 
