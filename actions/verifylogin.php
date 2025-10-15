96c0ba1094137e20e7814f3158900e110c892de8
ovictorqueiroz <victorbr442@gmail.com>
2025-10-14 23:52:45 -0300

    finalizando página de login

--- actions/verifylogin.php
+++ actions/verifylogin.php
@@ -2,8 +2,16 @@
     $pageName = "Login";
     include $_SERVER['DOCUMENT_ROOT'] . '/icecreamcompany/components/header.php';
 
+    $userEmail = $_POST['userEmail'];
+    $userPass = $_POST['userPass'];
 
-    echo "Teste bem sucedido!";
+    if($userEmail == "admin@gmail.com" && $userPass == 123){
+
+        echo "Bem-Vindo, $userEmail!";
+    }
+    else{
+        echo "Usuário ou Senha inválidos!";
+    }
 
     include $_SERVER['DOCUMENT_ROOT'] . '/icecreamcompany/components/footer.php';
 
