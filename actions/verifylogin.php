bbe33c6cf820390860a5dfe1cdd80778d3f811a8
ovictorqueiroz <victorbr442@gmail.com>
2025-10-14 23:47:03 -0300

    finalizando página de login

--- actions/verifylogin.php
+++ actions/verifylogin.php
@@ -1,4 +1,10 @@
 <?php
+    $pageName = "Login";
+    include $_SERVER['DOCUMENT_ROOT'] . '/icecreamcompany/components/header.php';
+
+
     echo "Teste bem sucedido!";
+
+    include $_SERVER['DOCUMENT_ROOT'] . '/icecreamcompany/components/footer.php';
 
 ?>
--- pages/login.php
+++ pages/login.php
@@ -5,10 +5,10 @@ include $_SERVER['DOCUMENT_ROOT'].'/icecreamcompany/components/header.php';?>
 <main>
     <h1>Login</h1>
     
-    <form action="../actions/verifylogin.php" method = "post">
-        <label for="userEmail">Email: </label> <input type="email" name="userEmail" placeholder = "Digite seu e-mail"> <br><br>
-        <label for="userPass">Senha: </label> <input type="password" name="userPass" placeholder = "Digite sua senha"> <br><br>
-        <input type="submit" name="btnEnviar">
+    <form action="../actions/verifylogin.php" method = "post" id = "loginForm">
+        <label for="userEmail">Email </label> <input type="email" name="userEmail" placeholder = "Digite seu e-mail" class = "inputField"> <br><br>
+        <label for="userPass">Senha </label> <input type="password" name="userPass" placeholder = "Digite sua senha" class = "inputField"> <br><br>
+        <input type="submit" name="btnEnviar" class = "btnForm"> <br><br>
         <p>Não possui uma conta? <a href = "../pages/cadastro.php">Cadastre-se</a></p>
     </form>
 </main>
