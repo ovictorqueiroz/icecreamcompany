01bc4330ec29aec3a7e220ea9878869b148815f3
ovictorqueiroz <victorbr442@gmail.com>
2025-10-17 23:42:24 -0300

    ajustes

--- pages/cadastro.php
+++ pages/cadastro.php
@@ -4,6 +4,14 @@ include $_SERVER['DOCUMENT_ROOT'].'/icecreamcompany/components/header.php';?>
 
 <main>
     <h1>Cadastro</h1>
+    <form action="../actions/verifylogin.php" method = "post" id = "loginForm">
+        <label for="userEmail">Nome de Usuário </label> <input type="email" name="userEmail" placeholder = "Digite seu nome de usuário" class = "inputField"> <br><br>
+        <label for="userEmail">Email </label> <input type="email" name="userEmail" placeholder = "Digite seu e-mail" class = "inputField"> <br><br>
+        <label for="userPass">Senha </label> <input type="password" name="userPass" placeholder = "Digite sua senha" class = "inputField"> <br><br>
+        <label for="userPass">Confirmação de Senha </label> <input type="password" name="userPass" placeholder = "Confirme sua senha" class = "inputField"> <br><br>
+        <input type="submit" name="btnEnviar" class = "btnForm"> <br><br>
+        <p>Já possui uma conta? <a href = "../pages/login.php">Entrar</a></p>
+    </form>
 </main>
 
 <?php 
--- pages/produtos.php
+++ pages/produtos.php
@@ -10,10 +10,10 @@ include $_SERVER['DOCUMENT_ROOT'] . '/icecreamcompany/components/header.php'; ?>
             <img src="../assets/mango-party-ii.png" class="card-img-top" alt="...">
             <div class="card-body">
                 <h4 class="card-title fw-bolder">Mango Party</h4>
-                <p class="card-text h-5 ">Explosão tropical de manga madura, cremosa e vibrante, com sabor refrescante e ensolarado.</p>
+                <p class="card-text h-5 ">Explosão tropical de manga, cremosa e vibrante, com sabor refrescante e ensolarado.</p>
                 <div class='d-flex gap-2'>
-                    <a href="#" class="btn btn-primary text-center h-2  ">Comprar</a>
-                    <a href="#" class="btn btn-primary text-center h-2  ">Adicionar ao carrinho</a>
+                    <a href="#" class="btn btn-primary text-center w-100 h-2  ">Saiba Mais</a>
+
                 </div>
             </div>
         </div>
@@ -24,40 +24,63 @@ include $_SERVER['DOCUMENT_ROOT'] . '/icecreamcompany/components/header.php'; ?>
             <div class="card-body">
                 <h4 class="card-title fw-bolder">Berry Lover</h4>
                 <p class="card-text h-5 ">Sorvete de morango suave e delicado, com textura cremosa e toque frutado irresistível.</p>
-                <div class='d-flex gap-2'>
-                    <a href="#" class="btn btn-primary align-middle text-center h-2  ">Comprar</a>
-                    <a href="#" class="btn btn-primary align-middle text-center h-2  ">Adicionar ao carrinho</a>
+                <div class='d-flex'>
+                    <a href="#" class="btn btn-primary align-middle text-center w-100 w-100 h-2  ">Saiba Mais</a>
                 </div>
             </div>
         </div>
-        
+
         <div class="card rainbow-sky" style="width: 18rem;">
             <img src="../assets/rainbow-sky.png" class="card-img-top" alt="...">
             <div class="card-body">
                 <h4 class="card-title fw-bolder">Rainbow Sky</h4>
-                <p class="card-text h-5 ">Delicioso tutti-frutti em tom azul ciano, leve e doce, com aroma divertido e nostálgico.</p>
+                <p class="card-text h-5 ">Delicioso Tutti-Frutti, leve e doce, com aroma divertido e nostálgico.</p>
                 <div class='d-flex gap-2'>
-                    <a href="#" class="btn btn-primary text-center h-2  ">Comprar</a>
-                    <a href="#" class="btn btn-primary text-center h-2  ">Adicionar ao carrinho</a>
+                    <a href="#" class="btn btn-primary text-center w-100 h-2  ">Saiba Mais</a>
+
                 </div>
             </div>
         </div>
-        
+
         <div class="card choc-muffin" style="width: 18rem;">
             <img src="../assets/choc-muffin.png" class="card-img-top" alt="...">
             <div class="card-body">
                 <h4 class="card-title fw-bolder">Choc Muffin</h4>
                 <p class="card-text h-5 ">Rico sorvete de chocolate cremoso, intenso e aveludado, com sabor de puro prazer.</p>
                 <div class='d-flex gap-2'>
-                    <a href="#" class="btn btn-primary text-center h-2  ">Comprar</a>
-                    <a href="#" class="btn btn-primary text-center h-2  ">Adicionar ao carrinho</a>
+                    <a href="#" class="btn btn-primary text-center w-100 h-2  ">Saiba Mais</a>
+
                 </div>
             </div>
         </div>
-        
+
+        <div class="card choc-muffin" style="width: 18rem;">
+            <img src="../assets/choc-muffin.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h4 class="card-title fw-bolder">Green Paradise</h4>
+                <p class="card-text h-5 ">Kiwi e limão-siciliano, azedinho e vibrante.</p>
+                <div class='d-flex gap-2'>
+                    <a href="#" class="btn btn-primary text-center w-100 h-2  ">Saiba Mais</a>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="card choc-muffin" style="width: 18rem;">
+            <img src="../assets/choc-muffin.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h4 class="card-title fw-bolder">Vanilla Dream</h4>
+                <p class="card-text h-5 ">Baunilha cremosa com notas suaves de leite e açúcar mascavo.</p>
+                <div class='d-flex gap-2'>
+                    <a href="#" class="btn btn-primary text-center w-100 h-2  ">Saiba Mais</a>
+
+                </div>
+            </div>
+        </div>
+
     </div>
 
- 
+
 </main>
 
 <?php
